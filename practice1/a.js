f14a4d3fdce792833de1733c54e33e36b1ec9a1f
nkbuggot <49551294+nkbuggot@users.noreply.github.com>
2019-12-19 14:32:25 +0800

    w

--- practice1/a.js
+++ practice1/a.js
@@ -5,6 +5,5 @@ node.appendChild(textnode);
 document.body.appendChild(node);
 //var element = document.getElementById("div1");
 //element.appendChild(para);
-<p>cccccccccccccccccssssssssssssssssss</p>
 
 }
