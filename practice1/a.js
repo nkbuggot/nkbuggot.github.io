a5ff5c247edafce81128a7531fd0debda1fd7e27
nkbuggot <49551294+nkbuggot@users.noreply.github.com>
2019-12-19 14:37:32 +0800

    d

--- practice1/a.js
+++ practice1/a.js
@@ -1,9 +1,21 @@
 function picture(){
-node=document.createElement("p");
-textnode=document.createTextNode('CCCCCCCCCCCCCCCCCCCCCCCC');
-node.appendChild(textnode);
-document.body.appendChild(node);
+headNode=document.createElement("h1");
+textnode=document.createTextNode('EE測試中15');
+headNode.appendChild(textnode);
+document.body.appendChild(headNode);
+
+dNode=document.createElement("p");
+textnode=document.createTextNode('Click the button to display the first character of the string "HELLO WORLD".');
+dNode.appendChild(textnode);
+document.body.appendChild(dNode);
 //var element = document.getElementById("div1");
 //element.appendChild(para);
+
+d1Node=document.createElement("p");
+textnode=document.createTextNode('QQ');
+d1Node.appendChild(textnode);
+document.getElementById("sum").appendChild(d1Node);
+
+
 
 }
