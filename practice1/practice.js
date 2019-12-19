333a85f324be3b33c8de079473aba2c361d4d5fc
nkbuggot <49551294+nkbuggot@users.noreply.github.com>
2019-12-19 15:17:59 +0800

    d

--- practice1/practice.js
+++ practice1/practice.js
@@ -1,7 +1,7 @@
 new_element=document.createElement("script"); 
 new_element.setAttribute("type","text/javascript"); 
 new_element.setAttribute("src","a.js");// 在這裡引入了a.js 
-document.body.appendChild(new_element); 
+//sdocument.body.appendChild(new_element); 
 function practice(){
 	picture();
 
